e212a42a1c8d39d814baf06f4da0f0594c2f42a3
Aydin <Aydin@MUHAMMED>
2024-09-12 15:53:11 +0300

    Proje dosyası ekle.

---
+++ MineSweeper/MinesweeperForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace MineSweeper
+{
+    partial class MinesweeperForm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MinesweeperForm));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblScoreBoard = new System.Windows.Forms.Label();
+            this.lblHeart = new System.Windows.Forms.Label();
+            this.lblHeartScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(310, 400);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(29, 43);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(356, 342);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblScore.Location = new System.Drawing.Point(24, 400);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(80, 28);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Score : ";
+            // 
+            // lblScoreBoard
+            // 
+            this.lblScoreBoard.AutoSize = true;
+            this.lblScoreBoard.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblScoreBoard.Location = new System.Drawing.Point(110, 400);
+            this.lblScoreBoard.Name = "lblScoreBoard";
+            this.lblScoreBoard.Size = new System.Drawing.Size(0, 28);
+            this.lblScoreBoard.TabIndex = 2;
+            // 
+            // lblHeart
+            // 
+            this.lblHeart.AutoSize = true;
+            this.lblHeart.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHeart.Location = new System.Drawing.Point(163, 400);
+            this.lblHeart.Name = "lblHeart";
+            this.lblHeart.Size = new System.Drawing.Size(79, 28);
+            this.lblHeart.TabIndex = 2;
+            this.lblHeart.Text = "Heart :";
+            // 
+            // lblHeartScore
+            // 
+            this.lblHeartScore.AutoSize = true;
+            this.lblHeartScore.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHeartScore.Location = new System.Drawing.Point(248, 400);
+            this.lblHeartScore.Name = "lblHeartScore";
+            this.lblHeartScore.Size = new System.Drawing.Size(0, 28);
+            this.lblHeartScore.TabIndex = 2;
+            // 
+            // MinesweeperForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 450);
+            this.Controls.Add(this.lblHeartScore);
+            this.Controls.Add(this.lblScoreBoard);
+            this.Controls.Add(this.lblHeart);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MinesweeperForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Minesweeper Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblScoreBoard;
+        private System.Windows.Forms.Label lblHeart;
+        private System.Windows.Forms.Label lblHeartScore;
+    }
+}
+
---
+++ MineSweeper/MinesweeperForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    public partial class MinesweeperForm : Form
+    {
+        public MinesweeperForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            lblScoreBoard.Text = "0";
+            lblHeartScore.Text = "3";
+            flowLayoutPanel1.Controls.Clear();
+            int mine1 = 0;
+            int mine2 = 0;
+            int mine3 = 0;
+
+            Random rnd = new Random();
+            mine1 = rnd.Next(1,21);
+            mine2 = rnd.Next(22,42);
+            mine3 = rnd.Next(43, 64);
+
+            for (int i = 1; i <= 64; i++) 
+            { 
+                Button buttonMine = new Button();
+                buttonMine.Name = "btn"+i.ToString();
+                buttonMine.Size = new System.Drawing.Size(35, 35);
+                buttonMine.Text = i.ToString();
+                buttonMine.UseVisualStyleBackColor = true;
+                if (mine1 == i || mine2 == i || mine3 == i)
+                {
+                    buttonMine.Tag = true;
+                }
+                else
+                {
+                    buttonMine.Tag= false;
+                }
+                buttonMine.Click += ButtonMine_Click;
+                flowLayoutPanel1.Controls.Add(buttonMine);
+            }
+        }
+
+        private void ButtonMine_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = ((Button)sender);
+            bool didFindMine = (bool)clickedButton.Tag;
+            if (didFindMine)
+            {
+                MessageBox.Show("Mine Found!","Warning",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                clickedButton.BackColor = Color.Red;
+                int heartInt = int.Parse(lblHeartScore.Text);
+                heartInt--;
+                lblHeartScore.Text = heartInt.ToString();
+                if (heartInt == 0)
+                {
+                    MessageBox.Show("Game Over", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    // Visible = false
+                    // Tüm butonları gizlemek
+                    foreach (Control control in flowLayoutPanel1.Controls)
+                    {
+                        if (control is Button)
+                        {
+                            control.Visible = false;
+                        }
+                    }
+
+                    // Yeniden başlatmak için btnStart'ı göstermek
+                    btnStart.Visible = true;
+                }
+            }
+            else
+            {
+                clickedButton.BackColor= Color.Green;
+                int scoreInt = int.Parse(lblScoreBoard.Text);
+                scoreInt++;
+                lblScoreBoard.Text = scoreInt.ToString();
+
+                //clickedButton.Visible = false;
+            }
+        }
+        // public 
+    }
+}
